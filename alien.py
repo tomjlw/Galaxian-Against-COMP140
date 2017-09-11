6576a72c30ef01ea28064fa41d98ef5966864b15
Liwen Jiang <tomjlw123@gmail.com>
2017-09-10 20:06:00 -0500

    Update alien.py

--- alien.py
+++ alien.py
@@ -4,7 +4,7 @@ from pygame.sprite import Sprite
 
 class Alien(Sprite):
     def __init__(self, ai_settings, screen):
-        super(Alien, self).__init__()365
+        super(Alien, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
